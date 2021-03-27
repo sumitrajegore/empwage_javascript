fecc03cb19cf594e63f2a30880a61073813ceeb8
Sumit Rajegore <sumitrajegorevp@gmail.com>
2021-03-27 15:24:12 +0530

    find total wage given empwage

--- employwage.js
+++ employwage.js
@@ -1,11 +1,24 @@
-    const IS_ABSENT = 0
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const IS_PART_TIME_HOURS = 4;
+    const IS_FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOUR = 20;
 
-    let empcheck = Math.floor(Math.random() * 10) % 2;
+    let empHrs=0;
+    let empcheck = Math.floor(Math.random() * 10) % 3;
     
-    if(empcheck == IS_ABSENT) {
+    switch(empcheck) {
 
-        console.log( "employ is absent");
-    } else {
-    
-        console.log( "employ is present");
-    }
+        case IS_PART_TIME :
+             empHrs=IS_PART_TIME_HOURS;
+             break;
+         
+        case IS_FULL_TIME :
+             empHrs=IS_FULL_TIME_HOURS;
+             break; 
+             
+        default:
+            empHrs=0;
+    }    
+    let empwage = empHrs * WAGE_PER_HOUR;
+    console.log("EMP WAGE IS : " +empwage);
