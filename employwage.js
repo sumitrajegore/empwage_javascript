301d02aa5ff84461f58ac1dc9db18b9156c4153a
Sumit Rajegore <sumitrajegorevp@gmail.com>
2021-03-27 15:05:48 +0530

    check employee is absent or not

---
+++ employwage.js
@@ -0,0 +1,11 @@
+    const IS_ABSENT = 0
+
+    let empcheck = Math.floor(Math.random() * 10) % 2;
+    
+    if(empcheck == IS_ABSENT) {
+
+        console.log( "employ is absent");
+    } else {
+    
+        console.log( "employ is present");
+    }
