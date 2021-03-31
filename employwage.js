9520bd3e0a6efa3c0c2deb92246dc0772903db58
Sumit Rajegore <sumitrajegorevp@gmail.com>
2021-03-31 14:39:24 +0530

    calculate totalempwage using array

--- employwage.js
+++ employwage.js
@@ -4,8 +4,16 @@
     const IS_FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20;
     const NUM_WORKING_DAY = 20;
-    const MAX_HRS_IN_MONTH = 100;
+    const MAX_HRS_IN_MONTH = 160;
+    let empHrs=0;
+    let totalworkingdays=0;
+    let totalEmpHrs=0;
+    let empDailywageArr= new Array();
 
+    function caLCDailywage(empHrs) {
+
+        return empHrs * WAGE_PER_HOUR;
+    }
     function getWorkingHours(empcheck){
 
     switch(empcheck) {
@@ -20,15 +28,13 @@
             return 0;
         }
     }
-
-    let empHrs=0;
-    let totalworkingdays=0;
     while( empHrs < MAX_HRS_IN_MONTH && totalworkingdays < NUM_WORKING_DAY) { 
     
     totalworkingdays++;
     let empcheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empcheck); 
+    empHrs = getWorkingHours(empcheck); 
+    totalEmpHrs += empHrs;
+    empDailywageArr.push(caLCDailywage(empHrs));
     }
-
-    let empwage = empHrs * WAGE_PER_HOUR;
-    console.log("Total Hours : " +empHrs +"    "+ "EMP WAGE IS : " +empwage+"   "+"total_day_month :" +totalworkingdays);
+    let empwage = caLCDailywage(totalEmpHrs);
+    console.log("Total Hours : " +totalEmpHrs +"    "+ "EMP WAGE IS : " +empwage+"   "+"total_day_month :" +totalworkingdays);
