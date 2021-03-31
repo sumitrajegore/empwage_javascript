0a2677ec3175d534573878d82e98949909cc8812
Sumit Rajegore <sumitrajegorevp@gmail.com>
2021-03-31 21:55:59 +0530

    find empwage using helper function

--- employwage.js
+++ employwage.js
@@ -12,6 +12,7 @@
 
     function caLCDailywage(empHrs) {
 
+        console.log(empHrs * WAGE_PER_HOUR);
         return empHrs * WAGE_PER_HOUR;
     }
     function getWorkingHours(empcheck){
@@ -36,5 +37,74 @@
     totalEmpHrs += empHrs;
     empDailywageArr.push(caLCDailywage(empHrs));
     }
-    let empwage = caLCDailywage(totalEmpHrs);
-    console.log("Total Hours : " +totalEmpHrs +"    "+ "EMP WAGE IS : " +empwage+"   "+"total_day_month :" +totalworkingdays);
+    let totalempwage = caLCDailywage(totalEmpHrs);
+    console.log(" Total Hours : " +totalEmpHrs +"    "+ "EMP WAGE IS : " +totalempwage+"   "+"total_day_month :" +totalworkingdays);
+
+    //UC7Aa
+    let totalempwages=0;
+    function sum(dailywage) {
+
+        //console.log(dailywage);
+        totalempwages += dailywage;
+    }
+    empDailywageArr.forEach(sum);
+    console.log("UC7A(a) Total Hours : " +totalEmpHrs +"    "+ "EMP WAGE IS : " +totalempwages+"   "+"total_day_month :" +totalworkingdays);
+
+    //UC7Ab
+    function totalempwagess(totalempwages, dailywage) {
+
+        //console.log( totalempwages + dailywage);
+        return(totalempwages + dailywage);
+    }
+    console.log("UC7A(b) empwage with reduse  :" + empDailywageArr.reduce(totalempwagess));
+
+    //UC7B
+    let dailycounter=0;
+    function mapdaywithwage(dailywage) {
+
+        dailycounter++;
+        return  "\n " + dailycounter + ' " ' + dailywage ; 
+    }
+    let mapdaywithwagearr = empDailywageArr.map(mapdaywithwage);
+    console.log("UC7B daily wage map : " + mapdaywithwagearr);
+
+    //UC7C
+    function fulltimewage(dailywage) {
+
+        return dailywage.includes("160");
+    }
+    let fulldaywagearr = mapdaywithwagearr.filter(fulltimewage);
+    console.log(" \n UC7C daily wage filter when fulltime wage earned  :" +fulldaywagearr);
+
+    //UC7D
+    function findfulltimewage(dailywage) {
+
+        return dailywage.includes("160");
+    }
+    console.log(" \n UC7D first daily wage filter when fulltime wage earned  :" + mapdaywithwagearr.find(findfulltimewage));
+
+    //UC7E
+    function isallfulltimewage(dailywage) {
+
+        return dailywage.includes("160");
+    }
+    console.log(" \n UC7E check all element have full time wage  : " + fulldaywagearr.every(isallfulltimewage));
+
+    //UC7F
+    function isanyparttimewage(dailywage) {
+
+        return dailywage.includes("80");
+    }
+    console.log(" \n UC7F check any  parttime wage earned  : " + mapdaywithwagearr.some(isanyparttimewage));
+
+    // UC7G find number of day employee worked
+    function totaldayworked(numofdays, dailywage) {
+
+        if(dailywage > 0)
+            return numofdays + 1;
+        return numofdays;
+    }
+    console.log("\n UC7G num of day emp worked : " + empDailywageArr.reduce(totaldayworked, 0));
+
+    //UC8
+    //console.log("\n UC8A empwage map total Hrs : " + Array.from(empDailywageMap.values()).reduce(totalempwage, 0));
